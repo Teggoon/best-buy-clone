031e29803440e972e3aeb038851a6226c39547b4
Teggoon <daniel.hc.li@gmail.com>
2022-03-30 21:34:12 -0700

    sass styling on buttonds

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <h2 className="logo-title">Quizlet</h2>
         <h3 className="nav-bar-text">Home</h3>
         <YourLibraryDropdown />
-        <button className="btn create-button navbar-button">Create</button>
+        <button className="btn create-button navbar-button">Create study set</button>
         <button className="btn upgrade-button navbar-button">Upgrade: Free 7-day trial</button>
     </div>);
 }
